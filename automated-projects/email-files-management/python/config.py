f68e01cad259f7551f7835215489e173aca76298
Olga Amitousa-Mankoy <olga.mankoy@gmail.com>
2025-03-23 17:11:25 +0100

    Add configs

---
+++ automated-projects/email-files-management/python/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# ===================================
+# Author: @aglorhythm
+# Configurations
+# ===================================
+
+from dotenv import load_dotenv
+import os
+import boto3
+
+# load environment variables
+load_dotenv()
